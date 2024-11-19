6261f9d4c23a650c71a42f30d87ff72a0eff1b1d
kamesh7773 <kameshsinghaaa64@gmail.com>
2024-11-19 10:01:31 +0530

    commit from home

--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -8,10 +8,42 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
+  // variable declaration
+  Color signInButtonColor = const Color.fromARGB(255, 53, 52, 52);
+  Color signInButtonTextColor = const Color.fromARGB(255, 124, 123, 123);
+
+  // textediting controllar's
+  TextEditingController emailControllar = TextEditingController();
+  TextEditingController passwordControllar = TextEditingController();
+
+  // called every time when textediting controllar begin used.
+  void listenPasswordTextEditingControllar() {
+    if (passwordControllar.value.text.isNotEmpty) {
+      setState(() {
+        signInButtonColor = const Color.fromARGB(255, 41, 116, 255);
+        signInButtonTextColor = const Color.fromARGB(255, 255, 255, 255);
+      });
+    }
+    if (passwordControllar.value.text.isEmpty) {
+      setState(() {
+        signInButtonColor = const Color.fromARGB(255, 53, 52, 52);
+        signInButtonTextColor = const Color.fromARGB(255, 124, 123, 123);
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // method that listen the Textediting Controllar.
+    passwordControllar.addListener(listenPasswordTextEditingControllar);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 36, 36, 36),
+      //! AppBar
       appBar: AppBar(
         title: const Text(
           "Sing in",
@@ -44,6 +76,7 @@ class _SignInPageState extends State<SignInPage> {
               ),
             ),
           ),
+          const SizedBox(height: 8),
           //! Email Textediting Controller
           Container(
             decoration: const BoxDecoration(
@@ -59,9 +92,12 @@ class _SignInPageState extends State<SignInPage> {
                 ),
               ),
             ),
-            child: const TextField(
+            child: TextField(
+              controller: emailControllar,
+              cursorColor: Colors.lightBlue,
+              style: const TextStyle(color: Colors.white),
               textAlign: TextAlign.center,
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 hintText: "Email",
                 hintStyle: TextStyle(color: Color.fromARGB(255, 124, 123, 123)),
                 enabledBorder: UnderlineInputBorder(borderSide: BorderSide.none),
@@ -84,9 +120,12 @@ class _SignInPageState extends State<SignInPage> {
                 ),
               ),
             ),
-            child: const TextField(
+            child: TextField(
+              controller: passwordControllar,
+              cursorColor: Colors.lightBlue,
+              style: const TextStyle(color: Colors.white),
               textAlign: TextAlign.center,
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 hintText: "Password",
                 hintStyle: TextStyle(
                   color: Color.fromARGB(255, 124, 123, 123),
@@ -103,19 +142,143 @@ class _SignInPageState extends State<SignInPage> {
               style: ElevatedButton.styleFrom(
                 shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
                 minimumSize: const Size(370, 46),
-                backgroundColor: const Color.fromARGB(255, 53, 52, 52),
-                foregroundColor: Colors.white,
+                backgroundColor: signInButtonColor,
               ),
-              onPressed: () {},
-              child: const Text(
-                "Sing in",
+              onPressed: () {
+                if (passwordControllar.value.text.isEmpty) {
+                } else {}
+              },
+              child: Text(
+                "Sign in",
                 style: TextStyle(
                   fontSize: 16,
-                  color: Color.fromARGB(255, 124, 123, 123),
+                  color: signInButtonTextColor,
                 ),
               ),
             ),
           ),
+          //! Forgot Password Button
+          Padding(
+            padding: const EdgeInsets.only(left: 24, top: 10),
+            child: InkWell(
+              borderRadius: BorderRadius.circular(8),
+              onTap: () {},
+              splashColor: const Color.fromARGB(4, 56, 192, 255),
+              child: const Padding(
+                padding: EdgeInsets.all(6),
+                child: Text(
+                  "Forgot password?",
+                  style: TextStyle(
+                    color: Color.fromRGBO(71, 135, 255, 1),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          //! Other Sing in methods
+          const Padding(
+            padding: EdgeInsets.only(left: 12, top: 40),
+            child: Text(
+              "Other sign in methods",
+              style: TextStyle(color: Color.fromARGB(255, 147, 144, 144), fontSize: 14),
+            ),
+          ),
+          const SizedBox(height: 40),
+          Center(
+            child: Column(
+              children: [
+                //! continue with google.
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    side: const BorderSide(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      width: 0.2,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    fixedSize: const Size(370, 52),
+                    backgroundColor: const Color.fromARGB(255, 55, 55, 55),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Image.asset("assets/images/google.png"),
+                      const Text(
+                        "Continue with Google",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                      const SizedBox()
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    side: const BorderSide(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      width: 0.2,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    fixedSize: const Size(370, 52),
+                    backgroundColor: const Color.fromARGB(255, 55, 55, 55),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Image.asset("assets/images/apple.png"),
+                      const Text(
+                        "Continue with Apple",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                      const SizedBox()
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    side: const BorderSide(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      width: 0.2,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    fixedSize: const Size(370, 52),
+                    backgroundColor: const Color.fromARGB(255, 55, 55, 55),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Image.asset(
+                        "assets/images/facebook.png",
+                      ),
+                      const Text(
+                        "Continue with Faceboook",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                      const SizedBox()
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
