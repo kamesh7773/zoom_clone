394074d5bd151fb0899acc0d4472ea1e4cc19588
kamesh7773 <kameshsinghaaa64@gmail.com>
2024-11-23 14:26:05 +0530

    form validation has complete

--- lib/pages/auth pages/create_account.dart
+++ lib/pages/auth pages/create_account.dart
@@ -1,5 +1,5 @@
+import 'package:colored_print/colored_print.dart';
 import 'package:flutter/material.dart';
-import 'package:zoom_clone/utils/password_validator.dart';
 
 class CreateAccount extends StatefulWidget {
   const CreateAccount({super.key});
@@ -15,6 +15,13 @@ class _CreateAccountState extends State<CreateAccount> {
   Color createAccountButtonTextColor = const Color.fromARGB(255, 124, 123, 123);
   bool viewPassword = false;
 
+  // varible for password validation UI.
+  bool allowConsecutiveWord = false;
+  bool isEightChartors = false;
+  bool atLeastOneNumber = false;
+  bool atLeastOneLowercase = false;
+  bool atLeastOneUppercase = false;
+
   // textediting controllar's
   TextEditingController firstNameControllar = TextEditingController();
   TextEditingController lastNameControllar = TextEditingController();
@@ -23,15 +30,108 @@ class _CreateAccountState extends State<CreateAccount> {
   // called every time when textediting controllar begin used.
   // Here we also validate the birth year textediting controllar for accepting the right birty year.
   listenPasswordTextEditingControllar() {
-    if (!RegExp(r'(?:\d{4,}|([a-zA-Z])\1{3,}|0123|1234|2345|3456|4567|5678|6789|7890|0987|9876|8765|7654|6543|5432|4321|abcd|qwert|qwer|bcde|cdef|defg|efgh|fghi|ghij|ijkl|jklm|klmn|lmno|mnop|nopq|opqr|pqrs|qrst|rstu|stuv|tuvw|uvwx|vwxy|wxyz|aaaa|bbbb|cccc|dddd)').hasMatch(passwordControllar.value.text) && passwordControllar.value.text.isNotEmpty) {
+    // if password texteding controllar is empty or validation for checking Consecutive/repeted Words are being used or not.
+    if (passwordControllar.value.text.isNotEmpty && RegExp(r'(?:\d{4,}|([a-zA-Z])\1{3,}|0123|1234|2345|3456|4567|5678|6789|7890|0987|9876|8765|7654|6543|5432|4321|abcd|qwert|qwer|bcde|cdef|defg|efgh|fghi|ghij|ijkl|jklm|klmn|lmno|mnop|nopq|opqr|pqrs|qrst|rstu|stuv|tuvw|uvwx|vwxy|wxyz|aaaa|bbbb|cccc|dddd)').hasMatch(passwordControllar.value.text)) {
       setState(() {
-        createAccountButtonColor = const Color.fromARGB(255, 41, 116, 255);
-        createAccountButtonTextColor = const Color.fromARGB(255, 255, 255, 255);
-      });
-    } else {
-      setState(() {
+        allowConsecutiveWord = false;
+
         createAccountButtonColor = const Color.fromARGB(255, 53, 52, 52);
         createAccountButtonTextColor = const Color.fromARGB(255, 124, 123, 123);
+      });
+    }
+    if (passwordControllar.value.text.isNotEmpty && !RegExp(r'(?:\d{4,}|([a-zA-Z])\1{3,}|0123|1234|2345|3456|4567|5678|6789|7890|0987|9876|8765|7654|6543|5432|4321|abcd|qwert|qwer|bcde|cdef|defg|efgh|fghi|ghij|ijkl|jklm|klmn|lmno|mnop|nopq|opqr|pqrs|qrst|rstu|stuv|tuvw|uvwx|vwxy|wxyz|aaaa|bbbb|cccc|dddd)').hasMatch(passwordControllar.value.text)) {
+      setState(() {
+        allowConsecutiveWord = true;
+
+        createAccountButtonColor = const Color.fromARGB(255, 53, 52, 52);
+        createAccountButtonTextColor = const Color.fromARGB(255, 124, 123, 123);
+      });
+    }
+
+    // validating atleast 8 chartors.
+    if (!RegExp(r'^.{8,}$').hasMatch(passwordControllar.value.text)) {
+      setState(() {
+        isEightChartors = false;
+
+        createAccountButtonColor = const Color.fromARGB(255, 53, 52, 52);
+        createAccountButtonTextColor = const Color.fromARGB(255, 124, 123, 123);
+      });
+    }
+    if (RegExp(r'^.{8,}$').hasMatch(passwordControllar.value.text)) {
+      setState(() {
+        isEightChartors = true;
+
+        createAccountButtonColor = const Color.fromARGB(255, 53, 52, 52);
+        createAccountButtonTextColor = const Color.fromARGB(255, 124, 123, 123);
+      });
+    }
+
+    // validating at lest 1 number in password.
+    if (!RegExp(r'.*\d+.*').hasMatch(passwordControllar.value.text)) {
+      setState(() {
+        atLeastOneNumber = false;
+
+        createAccountButtonColor = const Color.fromARGB(255, 53, 52, 52);
+        createAccountButtonTextColor = const Color.fromARGB(255, 124, 123, 123);
+      });
+    }
+    if (RegExp(r'.*\d+.*').hasMatch(passwordControllar.value.text)) {
+      setState(() {
+        atLeastOneNumber = true;
+
+        createAccountButtonColor = const Color.fromARGB(255, 53, 52, 52);
+        createAccountButtonTextColor = const Color.fromARGB(255, 124, 123, 123);
+      });
+    }
+
+    // validating at lest 1 lowercase in password.
+    if (!RegExp(r'.*[a-z]+.*').hasMatch(passwordControllar.value.text)) {
+      setState(() {
+        atLeastOneLowercase = false;
+
+        createAccountButtonColor = const Color.fromARGB(255, 53, 52, 52);
+        createAccountButtonTextColor = const Color.fromARGB(255, 124, 123, 123);
+      });
+    }
+    if (RegExp(r'.*[a-z]+.*').hasMatch(passwordControllar.value.text)) {
+      setState(() {
+        atLeastOneLowercase = true;
+
+        createAccountButtonColor = const Color.fromARGB(255, 53, 52, 52);
+        createAccountButtonTextColor = const Color.fromARGB(255, 124, 123, 123);
+      });
+    }
+
+    // validating at lest 1 uppercase in password.
+    if (!RegExp(r'.*[A-Z]+.*').hasMatch(passwordControllar.value.text)) {
+      setState(() {
+        atLeastOneUppercase = false;
+
+        createAccountButtonColor = const Color.fromARGB(255, 53, 52, 52);
+        createAccountButtonTextColor = const Color.fromARGB(255, 124, 123, 123);
+      });
+    }
+    if (RegExp(r'.*[A-Z]+.*').hasMatch(passwordControllar.value.text)) {
+      setState(() {
+        atLeastOneUppercase = true;
+
+        createAccountButtonColor = const Color.fromARGB(255, 53, 52, 52);
+        createAccountButtonTextColor = const Color.fromARGB(255, 124, 123, 123);
+      });
+    }
+
+    // when all condition where not applied then..
+    if (passwordControllar.value.text.isNotEmpty && !RegExp(r'(?:\d{4,}|([a-zA-Z])\1{3,}|0123|1234|2345|3456|4567|5678|6789|7890|0987|9876|8765|7654|6543|5432|4321|abcd|qwert|qwer|bcde|cdef|defg|efgh|fghi|ghij|ijkl|jklm|klmn|lmno|mnop|nopq|opqr|pqrs|qrst|rstu|stuv|tuvw|uvwx|vwxy|wxyz|aaaa|bbbb|cccc|dddd)').hasMatch(passwordControllar.value.text) && RegExp(r'^.{8,}$').hasMatch(passwordControllar.value.text) && RegExp(r'.*\d+.*').hasMatch(passwordControllar.value.text) && RegExp(r'.*[a-z]+.*').hasMatch(passwordControllar.value.text) && RegExp(r'.*[A-Z]+.*').hasMatch(passwordControllar.value.text)) {
+      ColoredPrint.warning("all condition where true");
+      setState(() {
+        allowConsecutiveWord = true;
+        isEightChartors = true;
+        atLeastOneNumber = true;
+        atLeastOneLowercase = true;
+        atLeastOneUppercase = true;
+
+        createAccountButtonColor = const Color.fromARGB(255, 41, 116, 255);
+        createAccountButtonTextColor = const Color.fromARGB(255, 255, 255, 255);
       });
     }
   }
@@ -189,7 +289,6 @@ class _CreateAccountState extends State<CreateAccount> {
                             ),
                           ),
                         ),
-
                         Padding(
                           padding: EdgeInsets.only(left: 30),
                           child: Column(
@@ -250,16 +349,171 @@ class _CreateAccountState extends State<CreateAccount> {
                       ],
                     )
 
-                  // if password feild is not empty then return the PasswordFeildValidator.
-                  : PasswordFieldValidator(
-                      minLength: 8,
-                      uppercaseCharCount: 1,
-                      lowercaseCharCount: 1,
-                      numericCharCount: 1,
-                      defaultColor: Colors.white,
-                      successColor: Colors.green,
-                      failureColor: Colors.red,
-                      controller: passwordControllar,
+                  // if password feild is not empty then we show the validation warnings & rules.
+                  : Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        //! Password validation warnings.
+                        const Padding(
+                          padding: EdgeInsets.only(left: 25, top: 10),
+                          child: Text(
+                            "Password must inclued at least:",
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+
+                        Padding(
+                          padding: const EdgeInsets.only(left: 25),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              //! 8 characters.
+                              Row(
+                                children: [
+                                  isEightChartors
+                                      ? const Icon(
+                                          Icons.verified,
+                                          size: 15,
+                                          color: Colors.green,
+                                        )
+                                      : const Icon(
+                                          Icons.cancel,
+                                          size: 15,
+                                          color: Colors.red,
+                                        ),
+                                  const SizedBox(width: 5),
+                                  const Text(
+                                    "8 characters",
+                                    style: TextStyle(
+                                      color: Color.fromARGB(255, 166, 163, 163),
+                                      fontSize: 13,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              //! 1 Numbers.
+                              Row(
+                                children: [
+                                  atLeastOneNumber
+                                      ? const Icon(
+                                          Icons.verified,
+                                          size: 15,
+                                          color: Colors.green,
+                                        )
+                                      : const Icon(
+                                          Icons.cancel,
+                                          size: 15,
+                                          color: Colors.red,
+                                        ),
+                                  const SizedBox(width: 5),
+                                  const Text(
+                                    "1 number",
+                                    style: TextStyle(
+                                      color: Color.fromARGB(255, 166, 163, 163),
+                                      fontSize: 13,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              //! 1 LowerCase letter.
+                              Row(
+                                children: [
+                                  atLeastOneLowercase
+                                      ? const Icon(
+                                          Icons.verified,
+                                          size: 15,
+                                          color: Colors.green,
+                                        )
+                                      : const Icon(
+                                          Icons.cancel,
+                                          size: 15,
+                                          color: Colors.red,
+                                        ),
+                                  const SizedBox(width: 5),
+                                  const Text(
+                                    "1 lowercase letter",
+                                    style: TextStyle(
+                                      color: Color.fromARGB(255, 166, 163, 163),
+                                      fontSize: 13,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              //! 1 uppercase letter.
+                              Row(
+                                children: [
+                                  atLeastOneUppercase
+                                      ? const Icon(
+                                          Icons.verified,
+                                          size: 15,
+                                          color: Colors.green,
+                                        )
+                                      : const Icon(
+                                          Icons.cancel,
+                                          size: 15,
+                                          color: Colors.red,
+                                        ),
+                                  const SizedBox(width: 5),
+                                  const Text(
+                                    "1 uppercase",
+                                    style: TextStyle(
+                                      color: Color.fromARGB(255, 166, 163, 163),
+                                      fontSize: 13,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(left: 25, top: 14),
+                          child: Text(
+                            "Password must not include:",
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+
+                        Padding(
+                          padding: const EdgeInsets.only(left: 30),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              allowConsecutiveWord
+                                  ? const Padding(
+                                      padding: EdgeInsets.only(top: 2),
+                                      child: Icon(
+                                        Icons.verified,
+                                        size: 15,
+                                        color: Colors.green,
+                                      ),
+                                    )
+                                  : const Padding(
+                                      padding: EdgeInsets.only(top: 2),
+                                      child: Icon(
+                                        Icons.cancel,
+                                        size: 15,
+                                        color: Colors.red,
+                                      ),
+                                    ),
+                              const SizedBox(width: 5),
+                              const Text(
+                                '4 or more consecutive or repeted characters\n(Examples :"1234", "abcd", "1111" or "qwert")',
+                                style: TextStyle(
+                                  color: Color.fromARGB(255, 166, 163, 163),
+                                  fontSize: 13,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
               const SizedBox(height: 40),
               //! Sing In Button.
